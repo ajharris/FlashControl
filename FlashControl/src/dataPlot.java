2eff9e6c18bd9ac82d4abea138b55a40403eca7b
Andrew Harris <andrew.harris@ryerson.ca>
2014-07-30 11:44:31 -0400

    attempting to push changes to dataPlot

--- FlashControl/src/dataPlot.java
+++ FlashControl/src/dataPlot.java
@@ -1,7 +1,23 @@
 import processing.core.PApplet;
 
 
+
 public class dataPlot extends PApplet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int freq, value;
+	public void setup(){
+		size(1100, 220);
+		background(0);
+		freq = 375;
+		value = 100;
+	}
 	
+	public void draw(){
+		stroke(255);
+		line(freq-100, height, freq-100, value);
+	}
 
 }
